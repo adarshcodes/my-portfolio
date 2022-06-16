cf9f6d2ea4e8942a316f99339c6b4dafd658cd65
adarshcodes <adarsh.lrnr@gmail.com>
2022-06-16 11:17:05 +0530

    added sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sidebar.scss";
+
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			<button className="ham-btn">
+				<span></span>
+			</button>
+		</div>
+	);
+}
+
+export default Sidebar;
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import ContactButton from "../components/contactButton/ContactButton";
+import Sidebar from "../components/sidebar/Sidebar";
+import Navigation from "../components/nav/Navigation";
 
 import "./home.styles.scss";
 
 function Home() {
 	return (
 		<div className="main-container" id="main-container">
+			<Sidebar />
+			<Navigation />
+
 			<h1 className="heading-primary name-heading">
-				Blue <br />
-				Blang <br />
-				Studio
+				Adarsh <br />
+				Pratap <br />
+				Singh
 			</h1>
 
 			<ContactButton />
